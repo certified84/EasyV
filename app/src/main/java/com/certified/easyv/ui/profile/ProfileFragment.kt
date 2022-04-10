ee41b9f5adb48c7cc517333faf756c70f479e017
Samson Achiaga <sirhumble84@gmail.com>
2022-04-11 00:08:57 +0100

    Added some functionality to the ProfileFragment.kt

--- app/src/main/java/com/certified/easyv/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/certified/easyv/ui/profile/ProfileFragment.kt
@@ -14,6 +14,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.edit
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -100,6 +101,10 @@ class ProfileFragment : Fragment(), View.OnClickListener {
                             isChecked
                         )
                     }
+                    if (isChecked)
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    else
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
                 }
             }
         }
